c03365b4bbc54d7876c3f22a00462e8b34497088
Ben Houston <BenjaminAHouston@gmail.com>
2018-11-10 18:03:10 -0500

    Wrote the add inventory function

--- bamazonManager.js
+++ bamazonManager.js
@@ -14,7 +14,7 @@ connection.connect(err => {
 });
 
 function mainMenu() {
-    let options = ["View Products for Sale", "View Low Inventory", "Add to inventory", "Add New Product"];
+    let options = ["View Products for Sale", "View Low Inventory", "Add to inventory", "Add New Product", "Exit"];
     inquirer.prompt([
         {
             name: "options",
@@ -35,22 +35,66 @@ function mainMenu() {
             case options[3]:
                 addProduct();
                 break;
+            case options[4]:
+                connection.end();
+                break;
         }
-        connection.end();
     });
 }
 
 function viewProducts() {
-    connection.query("SELECT item_id, product_name, price, stock_quantity FROM `products`", (err, res) => {
+    connection.query('SELECT item_id, product_name, price, stock_quantity FROM `products`', (err, res) => {
         if (err) throw err;
         console.table(res);
+        mainMenu();
     });
 }
 
 function viewLowInventory() {
-    connection.query("SELECT * FROM `products` WHERE stock_quantity < 5", (err, res) => {
+    connection.query('SELECT * FROM `products` WHERE stock_quantity < 5', (err, res) => {
         if (err) throw err;
         console.table(res);
+        mainMenu();
+    });
+}
+
+function addInventory() {
+    inquirer.prompt([
+        {
+            name: 'id',
+            type: 'input',
+            message: 'Please enter the ID of the product you would like to stock to',
+            validate: function(input) {
+                input  = parseInt(input);
+                if (input > 0) {
+                    return true;
+                }
+                return "Please input a valid ID.";
+            }
+        },
+        {
+            name: 'amount',
+            type: 'input',
+            message: 'Please enter the amount of stock you would like to add to the product',
+            validate: function(input) {
+                input  = parseInt(input);
+                if (input >= 0) {
+                    return true;
+                }
+                return "Please input a valid amount.";
+            }
+        }
+    ]).then(response => {
+        let id = parseInt(response.id);
+        let amount = parseInt(response.amount);
+        connection.query('SELECT stock_quantity FROM `products` WHERE ?', {item_id: id}, (err, res) => {
+            if (err) throw err;
+            connection.query('UPDATE `products` SET ? WHERE ?', [{stock_quantity: res[0].stock_quantity + amount}, {item_id: id}], err => {
+                if (err) throw err;
+                console.log("Stock updated succeessfully!");
+                mainMenu();
+            });
+        });
     });
 }
 
