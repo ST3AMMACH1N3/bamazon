63d3aaaa4f35a489aea4490d33c1b80e7d8233c0
Ben Houston <BenjaminAHouston@gmail.com>
2018-11-11 17:20:52 -0500

    Wrote the create department function

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -38,7 +38,7 @@ function mainMenu() {
 }
 
 function viewSales() {
-    connection.query('SELECT departments.department_id, departments.department_name, departments.over_head_costs, products.price, products.units_sold  FROM `departments` INNER JOIN `products` ON departments.department_name = products.department_name', (err, res) => {
+    connection.query('SELECT departments.department_id, departments.department_name, departments.over_head_costs, products.price, products.units_sold  FROM `departments` LEFT JOIN `products` ON departments.department_name = products.department_name', (err, res) => {
         if (err) throw err;
         let table = [];
         let departments = [];
@@ -63,7 +63,40 @@ function viewSales() {
             }
         });
         console.table(table);
-        connection.end();
+        mainMenu();
+    });
+}
+
+function createDepartment() {
+    inquirer.prompt([
+        {
+            name: "name",
+            type: "input",
+            message: "Please input the name of the department being added",
+            validate: function(input) {
+                if (input.length > 1) {
+                    return true;
+                }
+                return "Please enter a valid department name"
+            }
+        },
+        {
+            name: "costs",
+            type: "input",
+            message: "Please enter the overhead costs of the department being added",
+            validate: function(input) {
+                if (parseFloat(input) >= 0) {
+                    return true;
+                }
+                return "Please enter a valid overhead cost"
+            }
+        }
+    ]).then(response => {
+        connection.query('INSERT INTO `departments` SET ?', {department_name: response.name, over_head_costs: parseFloat(response.costs).toFixed(2)}, (err, res) => {
+            if (err) throw err;
+            console.log("Department added successfully!");
+            mainMenu();
+        });
     });
 }
 
